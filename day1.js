956eea9053466efcae3cdb51cda1f684df8918bd
anshika494 <anshi040904@gmail.com>
2024-07-19 22:14:03 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,47 @@
+// Task 1
+var num = 10;
+console.log(num);
+
+//Task 2
+let say = "Hello, world!";
+console.log(say);
+
+//Task 3
+const result = true;
+console.log(result);
+
+//Task 4
+  // Number
+const num = 42;
+console.log("Number:", num);
+
+  // String
+const message = "This is a string.";
+console.log("String:", message);
+
+  // Boolean
+const isTrue = true;
+console.log("Boolean:", isTrue);
+
+  // Object
+const person = {
+  name: "Anshika",
+  age: 30,
+};
+console.log("Object:", person);
+
+  // Array
+const numbers = [1, 2, 3, 4, 5];
+console.log("Array:", numbers);
+
+//Task 5
+let count = 56;
+console.log("Initial value:", count);
+
+count = 45;
+console.log("New value:", count);
+
+//Task 6
+const age = 30;
+age = 35; // Will cause an error
+console.log(age);
